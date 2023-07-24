7593590effb8cd873a89f36ae00ed9480e95a617
Pikachú <engbethany4@gmail.com>
2023-07-24 18:07:46 +0300

    code updated

--- webapp/main.py
+++ webapp/main.py
@@ -6,6 +6,7 @@ from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
+import hashlib
 
 current_dir = dirname(abspath(__file__))
 static_path = join(current_dir, "static")
@@ -13,9 +14,12 @@ static_path = join(current_dir, "static")
 app = FastAPI()
 app.mount("/ui", StaticFiles(directory=static_path), name="ui")
 
+DEFAULT_LENGTH = 20
+
 
 class Body(BaseModel):
-    length: Union[int, None] = 20
+    length: Union[int, None] = DEFAULT_LENGTH
+    text: str
 
 
 @app.get('/')
@@ -33,5 +37,24 @@ def generate(body: Body):
         "length": 20
     }
     """
-    string = base64.b64encode(os.urandom(64))[:body.length].decode('utf-8')
+    try:
+        string = base64.b64encode(os.urandom(64))[:body.length].decode('utf-8')
+    except Exception as e:
+        return {'error': str(e)}
     return {'token': string}
+
+
+@app.post('/checksum')
+def checksum(body: Body):
+    """
+    Generate a checksum of the text. Example POST request body:
+
+    {
+        "text": "Hello World!"
+    }
+    """
+    try:
+        checksum = hashlib.sha256(body.text.encode('utf-8')).hexdigest()
+    except Exception as e:
+        return {'error': str(e)}
+    return {'checksum': checksum}
